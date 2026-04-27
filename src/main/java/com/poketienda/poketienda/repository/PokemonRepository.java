5a3643aded4074c6c866848b46352012f36ff051
pandequeso2 <ben.arayag@duocuc.cl>
2026-04-27 17:49:46 -0400

    ahora si xddd

--- src/main/java/com/poketienda/poketienda/repository/PokemonRepository.java
+++ src/main/java/com/poketienda/poketienda/repository/PokemonRepository.java
@@ -1,18 +1,12 @@
 package com.poketienda.poketienda.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.poketienda.poketienda.model.Pokemon;
 
-
 public interface PokemonRepository extends JpaRepository<Pokemon, Integer> {
-    List<Pokemon> findByActivoTrue();
-
-    void setNombre(String nombre);
-
-    void setPeso(String peso);
-
-    void setTipo(String tipo);
+    
+    // Solo dejamos los métodos que sirven para BUSCAR
+    List<Pokemon> findByPeso(String peso);
+    
 }
